fd5ee73c192f806696a25f60620124b5eab98003
IMBEKA <imbeka06@gmail.com>
2026-04-10 01:48:08 +0300

    Add Models route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'; // t
 import Transactions from './pages/Transaction';
 import FraudNetwork from './pages/FraudNetwork'; 
 import Alerts from './pages/Alerts';
+import Models from './pages/Models';
 import Reports from './pages/Reports';
 import Settings from './pages/Settings';
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/transactions" element={<Transactions />} />
           <Route path="/network" element={<FraudNetwork />} />
           <Route path="/alerts" element={<Alerts />} />
+          <Route path="/models" element={<Models />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="*" element={<div>Page under construction</div>} />        </Routes>
